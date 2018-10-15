944d264b7e5910e2e15d880773f40fdfe42deb33
DESKTOP-IHHUNT6\i5 <ellenmayaraweb@gmail.com>
2018-10-15 14:22:25 -0300

    colors variable

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,19 +1,21 @@
-var circleX = 0;
-var circleY = 0;
-var circleZ = 100;
-
+var r = 0;
+var b = 255;
+var circle = {x :0 , y: 200, diameter : 50};
 function setup() {
     createCanvas(700, 300);
 }
 
 function draw() {
-    background(100);
-
+    //oq sera mapeado,
+    // min e max do alcance(tamanho da tela),
+    // min e max intervalo
+    r = map(mouseX, 0, 600 , 0 , 255);
+    b = map(mouseY, 0, 600 , 0 , 255);
+    background(r, 0 , b);
     fill(250, 200, 200);
-    ellipse(circleX, circleY, circleZ, 80);
+    ellipse(mouseX, mouseY, circle.diameter, circle.diameter);
 
-    circleX ++;
-    circleY ++;
-    circleZ ++;
+    circle.x ++;
+
 
 }

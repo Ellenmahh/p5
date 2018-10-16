31e01e96e855ef0638958482925b2cadaf691864
DESKTOP-IHHUNT6\i5 <ellenmayaraweb@gmail.com>
2018-10-16 19:02:24 -0300

    offthescreen

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,17 +1,19 @@
-var spot = {x:100, y:50};
-var col = {r: 255, g:0, b:0};
+var x =0;
 function setup() {
     createCanvas(600, 400);
-    background(0);
+
 }
 
 function draw() {
-    spot.x= random(0,width);
-    spot.y= random(0,height);
-    col.r = random(0, 255);
-    col.g = random(0);
-    col.b = random(100, 190);
-    noStroke();
-    fill(col.r, col.g, col.b);
-    ellipse(spot.x, spot.y, 24, 24);
+    background(0);
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(x, 200, 100, 100);
+
+    if (x > width){
+        println("OFF THE SCREEN!");
+    }
+    x=x+3;
+
 }
